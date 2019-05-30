dd54a856b24a727bb15ba3730fe7ac6d7c938833
Ryan Rickert <RyanCRickert@gmail.com>
2019-05-30 11:05:22 -0500

    Added a bit of responsiveness to the tags

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,15 @@ module.exports = env => {
         {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ],
+            plugins: [
+              "@babel/plugin-proposal-class-properties"
+            ]
           }
         },
         {
